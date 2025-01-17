dec885d3c9b71c6bfb19cff7a1ff967804ff402e
louis.molinier <louis.molinier@epita.fr>
2025-01-17 22:31:42 +0100

    fix: Run a program

--- src/exec_non_builtin.rs
+++ src/exec_non_builtin.rs
@@ -7,11 +7,16 @@ fn is_in_folder(folder: &str, arg: &str) -> bool {
 }
 
 pub fn exec_non_builtin(args: Vec<&str>, paths: Vec<&str>) {
+    let command = args[0];
     let command_args = args[1..].to_vec();
     let mut found = false;
     for folder in &paths {
-        if is_in_folder(folder, args[0]) {
-            let _ = Command::new(args[0]).args(command_args).spawn();
+        if is_in_folder(folder, command) {
+            let _ = Command::new(command)
+                .args(command_args)
+                .spawn()
+                .expect("")
+                .wait();
             found = true;
             break;
         }
