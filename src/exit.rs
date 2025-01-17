530e05de1514867ae099d88f9192698825748587
louis.molinier <louis.molinier@epita.fr>
2025-01-17 19:33:20 +0100

    The exit builtin

---
+++ src/exit.rs
@@ -0,0 +1,15 @@
+use std::process::ExitCode;
+
+pub fn exit(args: Vec<&str>) -> (bool, ExitCode) {
+    match args.len() {
+        1 => return (true, ExitCode::from(0)),
+        2 => match args[1].parse::<u8>() {
+            Ok(code) => return (true, ExitCode::from(code)),
+            Err(_) => return (true, ExitCode::from(0)),
+        },
+        _ => {
+            println!("exit: too many arguments");
+            return (false, ExitCode::from(0));
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,31 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
+use std::process::ExitCode;
 
-fn main() {
+mod exit;
+use exit::exit;
+
+fn main() -> ExitCode {
     loop {
         print!("$ ");
         io::stdout().flush().unwrap();
 
-        // Wait for user input
         let stdin = io::stdin();
         let mut input = String::new();
         stdin.read_line(&mut input).unwrap();
         input.pop();
 
-        let args: Vec<&str> = input.split(" ").collect();
-        println!("{}: command not found", args[0]);
+        let args: Vec<&str> = input.split_whitespace().collect();
+        if args.len() != 0 {
+            match args[0] {
+                "exit" => {
+                    let exit_result = exit(args);
+                    if exit_result.0 {
+                        return exit_result.1;
+                    }
+                }
+                _ => println!("{}: command not found", args[0]),
+            }
+        }
     }
 }
