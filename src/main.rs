2928aeb80623ded1962a0f860c023a4c3645daaa
louis.molinier <louis.molinier@epita.fr>
2025-01-17 18:45:59 +0100

    REPL

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,17 @@
 use std::io::{self, Write};
 
 fn main() {
-    print!("$ ");
-    io::stdout().flush().unwrap();
+    loop {
+        print!("$ ");
+        io::stdout().flush().unwrap();
 
-    // Wait for user input
-    let stdin = io::stdin();
-    let mut input = String::new();
-    stdin.read_line(&mut input).unwrap();
-    input.pop();
+        // Wait for user input
+        let stdin = io::stdin();
+        let mut input = String::new();
+        stdin.read_line(&mut input).unwrap();
+        input.pop();
 
-    let args: Vec<&str> = input.split(" ").collect();
-    println!("{}: command not found", args[0]);
+        let args: Vec<&str> = input.split(" ").collect();
+        println!("{}: command not found", args[0]);
+    }
 }
