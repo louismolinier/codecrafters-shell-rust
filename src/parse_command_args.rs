c6da2ea3c66b3c7bdbb4bfec0563cc595ccc6425
louis.molinier <louis.molinier@epita.fr>
2025-01-26 21:19:51 +0100

    Double quotes

--- src/parse_command_args.rs
+++ src/parse_command_args.rs
@@ -29,6 +29,27 @@ pub fn parse_command_args(input: &str) -> Vec<String> {
             }
             result.push(temp);
             j += 1;
+        } else if input.chars().nth(i).unwrap() == '"' {
+            i += 1;
+            j = i;
+            while input.chars().nth(j).unwrap() != '"' {
+                j += 1;
+                if input.chars().nth(j).unwrap() == '"'
+                    && j + 1 < input.len()
+                    && input.chars().nth(j + 1).unwrap() == '"'
+                {
+                    j += 2;
+                }
+            }
+            let s = &input[i..j];
+            let mut temp = String::new();
+            for c in s.chars() {
+                if c != '"' {
+                    temp.push(c);
+                }
+            }
+            result.push(temp);
+            j += 1;
         } else {
             j = i;
             while j < input.len() && input.chars().nth(j).unwrap() != ' ' {
